ce9df5d9b03c869eae00911aa240ca7a926bebe6
Max S <creatingalong@gmail.com>
2020-01-27 18:23:16 -0600

    check to make sure passlengh is not blank check to make sure at least one checkbox checked allowed use of a word in password

--- PasswordGen/Form1.Designer.cs
+++ PasswordGen/Form1.Designer.cs
@@ -35,15 +35,17 @@
 			this.btnSubmit = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
-			this.resultsBox = new System.Windows.Forms.TextBox();
 			this.label3 = new System.Windows.Forms.Label();
 			this.txtPassLength = new System.Windows.Forms.TextBox();
+			this.resultsBox = new System.Windows.Forms.RichTextBox();
+			this.wordBox = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// uppercaseLetters
 			// 
 			this.uppercaseLetters.AutoSize = true;
-			this.uppercaseLetters.Location = new System.Drawing.Point(292, 61);
+			this.uppercaseLetters.Location = new System.Drawing.Point(207, 28);
 			this.uppercaseLetters.Name = "uppercaseLetters";
 			this.uppercaseLetters.Size = new System.Drawing.Size(113, 17);
 			this.uppercaseLetters.TabIndex = 0;
@@ -53,7 +55,7 @@
 			// lowercaseLetters
 			// 
 			this.lowercaseLetters.AutoSize = true;
-			this.lowercaseLetters.Location = new System.Drawing.Point(292, 84);
+			this.lowercaseLetters.Location = new System.Drawing.Point(207, 51);
 			this.lowercaseLetters.Name = "lowercaseLetters";
 			this.lowercaseLetters.Size = new System.Drawing.Size(113, 17);
 			this.lowercaseLetters.TabIndex = 1;
@@ -63,7 +65,7 @@
 			// specialChars
 			// 
 			this.specialChars.AutoSize = true;
-			this.specialChars.Location = new System.Drawing.Point(292, 130);
+			this.specialChars.Location = new System.Drawing.Point(207, 97);
 			this.specialChars.Name = "specialChars";
 			this.specialChars.Size = new System.Drawing.Size(91, 17);
 			this.specialChars.TabIndex = 3;
@@ -73,7 +75,7 @@
 			// Numbers
 			// 
 			this.Numbers.AutoSize = true;
-			this.Numbers.Location = new System.Drawing.Point(292, 107);
+			this.Numbers.Location = new System.Drawing.Point(207, 74);
 			this.Numbers.Name = "Numbers";
 			this.Numbers.Size = new System.Drawing.Size(68, 17);
 			this.Numbers.TabIndex = 2;
@@ -82,7 +84,7 @@
 			// 
 			// btnSubmit
 			// 
-			this.btnSubmit.Location = new System.Drawing.Point(46, 131);
+			this.btnSubmit.Location = new System.Drawing.Point(43, 150);
 			this.btnSubmit.Name = "btnSubmit";
 			this.btnSubmit.Size = new System.Drawing.Size(100, 23);
 			this.btnSubmit.TabIndex = 4;
@@ -93,7 +95,7 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(289, 27);
+			this.label1.Location = new System.Drawing.Point(207, 9);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(125, 13);
 			this.label1.TabIndex = 5;
@@ -102,23 +104,16 @@
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(40, 27);
+			this.label2.Location = new System.Drawing.Point(40, 9);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(106, 13);
 			this.label2.TabIndex = 6;
 			this.label2.Text = "Generated Password";
 			// 
-			// resultsBox
-			// 
-			this.resultsBox.Location = new System.Drawing.Point(12, 43);
-			this.resultsBox.Name = "resultsBox";
-			this.resultsBox.Size = new System.Drawing.Size(176, 20);
-			this.resultsBox.TabIndex = 7;
-			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
-			this.label3.Location = new System.Drawing.Point(54, 84);
+			this.label3.Location = new System.Drawing.Point(54, 108);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(73, 13);
 			this.label3.TabIndex = 8;
@@ -126,19 +121,45 @@
 			// 
 			// txtPassLength
 			// 
-			this.txtPassLength.Location = new System.Drawing.Point(46, 105);
+			this.txtPassLength.Location = new System.Drawing.Point(43, 124);
 			this.txtPassLength.Name = "txtPassLength";
 			this.txtPassLength.Size = new System.Drawing.Size(100, 20);
 			this.txtPassLength.TabIndex = 9;
+			// 
+			// resultsBox
+			// 
+			this.resultsBox.Location = new System.Drawing.Point(26, 26);
+			this.resultsBox.Name = "resultsBox";
+			this.resultsBox.Size = new System.Drawing.Size(142, 79);
+			this.resultsBox.TabIndex = 10;
+			this.resultsBox.Text = "";
+			// 
+			// wordBox
+			// 
+			this.wordBox.Location = new System.Drawing.Point(210, 152);
+			this.wordBox.Name = "wordBox";
+			this.wordBox.Size = new System.Drawing.Size(122, 20);
+			this.wordBox.TabIndex = 11;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(210, 133);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(110, 13);
+			this.label4.TabIndex = 12;
+			this.label4.Text = " Add a word (optional)";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(466, 185);
+			this.ClientSize = new System.Drawing.Size(367, 185);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.wordBox);
+			this.Controls.Add(this.resultsBox);
 			this.Controls.Add(this.txtPassLength);
 			this.Controls.Add(this.label3);
-			this.Controls.Add(this.resultsBox);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.btnSubmit);
@@ -162,9 +183,11 @@
 		private System.Windows.Forms.Button btnSubmit;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.TextBox resultsBox;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.TextBox txtPassLength;
+		private System.Windows.Forms.RichTextBox resultsBox;
+		private System.Windows.Forms.TextBox wordBox;
+		private System.Windows.Forms.Label label4;
 	}
 }
 
--- PasswordGen/Form1.cs
+++ PasswordGen/Form1.cs
@@ -20,50 +20,91 @@ namespace PasswordGen
 		private void btnSubmit_Click(object sender, EventArgs e)
 		{
 
+			//creates variables
 			string chars1 = "";
 			string chars2 = "";
 			string chars3 = "";
 			string chars4 = "";
-			var passLength = Convert.ToInt32(txtPassLength.Text);
+
+			//passLength converted to int for use in for loop
+			string passLength = txtPassLength.Text;
 			string passCombo;
 			string results = "";
-
-
-			if (specialChars.Checked)
+			
+			//checks to make sure at least one box is checked
+			if (specialChars.Checked == false && lowercaseLetters.Checked == false && uppercaseLetters.Checked == false && Numbers.Checked == false)
 			{
-				chars2 = "!@#$%^&*";
+				MessageBox.Show("Please check at least one of the boxes.","Error");
+			
+				//checks to make sure a password length was entered
+			} else if(string.IsNullOrEmpty(txtPassLength.Text)) {
+
+				MessageBox.Show("Please enter a length for you're password.", "Error");
+
+			} else {
+
+				//sets chars vars based on checkboxes
+				if (specialChars.Checked)
+				{
+					chars2 = "!@#$%^&*";
+				}
+
+				if (lowercaseLetters.Checked)
+				{
+					chars4 = "abcdefghijklmnopqrstuvwxyz";
+				}
+
+				if (uppercaseLetters.Checked)
+				{
+					chars3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				}
+
+				if (Numbers.Checked)
+				{
+					chars1 = "1234567890";
+				}
+
+				//combines all strings into 1 string
+				passCombo = String.Concat(chars1, chars2, chars3, chars4);
+
+				//converts string to array
+				char[] charArr = passCombo.ToCharArray();
+
+				Random rnd = new Random();
+
+				var totalElements = charArr.Count();
+
+				var passLengthNum = Convert.ToInt32(passLength);
+
+				//runs as many times as passwordLength
+				for (int i = 0; i < passLengthNum; i++)
+				{
+					int num = rnd.Next(0, totalElements);
+
+					//runs once halfway through the for loop
+					if (i == passLengthNum / 2)
+					{
+						if (string.IsNullOrEmpty(wordBox.Text))
+						{
+							//does nothing if empty
+						}
+						else
+						{
+							//ads the optional word to final pass
+							results = String.Concat(results, wordBox.Text);
+						}
+							
+					}
+
+					//adds a character to the results
+					results = String.Concat(results, charArr[num]);
+				}
+
+				//prints out the final password
+				resultsBox.Text = results;
 			}
 
-			if (lowercaseLetters.Checked)
-			{
-				chars4 = "abcdefghijklmnopqrstuvwxyz";
-			}
-
-			if (uppercaseLetters.Checked)
-			{
-				chars3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			}
-
-			if (Numbers.Checked)
-			{
-				chars1 = "1234567890";
-			}
-
-			passCombo = String.Concat(chars1, chars2, chars3, chars4);
-
-			char[] charArr = passCombo.ToCharArray();
-
-			Random rnd = new Random();
-
-			var totalElements = charArr.Count();
-
-			for (int i = 0; i < passLength; i++)
-			{
-				int num = rnd.Next(0, totalElements);
-				results = String.Concat(results, charArr[num]);
-			}
-
-			resultsBox.Text = results;
+			
 		}
 	}
 }
